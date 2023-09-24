c73ec507c2025fef603e449788d54cf0b7b0dbe1
Jonathan Marler <johnnymarler@gmail.com>
2023-09-23 23:28:27 -0600

    handle some types of errors like invalid string literal

--- zigscript.zig
+++ zigscript.zig
@@ -1,18 +1,16 @@
 const builtin = @import("builtin");
 const std = @import("std");
 
-const global = struct {
-    var error_arena_instance = std.heap.ArenaAllocator.init(std.heap.page_allocator);
-    pub const error_arena = error_arena_instance.allocator();
-};
-
-
 pub fn main() !void {
-    try testError("@badBuiltin()", "unknown builtin '@badBuiltin'");
+    try testError("@badBuiltin()", "unknown builtin");
     try testError("@out()", "@out requires 1 argument but got 0");
     try testError("@out(0)", "@out requires argument 1 to be of type string but got number");
     try testExpr(
         \\@out("Hello, World!\n")
+    );
+    try testError(
+        \\@out("\?")
+        , "invalid string literal",
     );
 }
 
@@ -20,52 +18,102 @@ pub fn oom(e: error{OutOfMemory}) noreturn {
     @panic(@errorName(e));
 }
 
+const TokenError = enum {
+    unknown_builtin,
+    invalid_string_literal,
+};
+const VmError = struct {
+    pos: usize,
+    kind: union(enum) {
+        token: TokenError,
+        general: []const u8,
+    },
+
+    pub fn deinit(self: VmError, allocator: std.mem.Allocator) void {
+        switch (self.kind) {
+            .token => {},
+            .general => |msg| allocator.free(msg),
+        }
+    }
+
+    pub fn getTestMsg(self: VmError) []const u8 {
+        switch (self.kind) {
+            .token => |token_error| switch (token_error) {
+                .unknown_builtin => return "unknown builtin",
+                .invalid_string_literal => return "invalid string literal",
+            },
+            .general => |msg| return msg,
+        }
+    }
+};
+
 const Vm = struct {
     src: [:0]const u8,
     allocator: std.mem.Allocator,
     stack: std.ArrayListUnmanaged(Value) = .{},
+    err: ?VmError = null,
 
     pub fn deinit(self: *Vm) void {
+        if (self.err) |err| {
+            err.deinit(self.allocator);
+        }
         for (self.stack.items) |item| {
             item.deinit(self.allocator);
         }
         self.stack.deinit(self.allocator);
     }
 
-    pub fn push_string_literal(self: *Vm, loc: std.zig.Token.Loc) void {
-        self.stack.append(self.allocator, .{
-            // TODO: escape the string literal
-            .string = self.allocator.dupe(u8, self.src[loc.start..loc.end]) catch |e| oom(e),
-        }) catch |e| oom(e);
+    fn tokenError(self: *Vm, token_pos: usize, token_error: TokenError) error{Vm} {
+        std.debug.assert(self.err == null);
+        self.err = .{ .pos = token_pos, .kind = .{ .token = token_error } };
+        return error.Vm;
+    }
+    fn generalError(self: *Vm, pos: usize, comptime fmt: []const u8, args: anytype) error{Vm} {
+        std.debug.assert(self.err == null);
+        self.err = .{
+            .pos = pos,
+            .kind = .{
+                .general = std.fmt.allocPrint(self.allocator, fmt, args) catch |e| oom(e),
+            },
+        };
+        return error.Vm;
     }
 
-    pub fn push_number_literal(self: *Vm, loc: std.zig.Token.Loc) void {
+    pub fn push_string_literal(self: *Vm, loc: std.zig.Token.Loc) error{Vm}!void {
+        const token = self.src[loc.start..loc.end];
+        const string = std.zig.string_literal.parseAlloc(self.allocator, token) catch |err| switch (err) {
+            error.OutOfMemory => |e| oom(e),
+            error.InvalidLiteral => return self.tokenError(loc.start, .invalid_string_literal),
+        };
+        self.stack.append(self.allocator, .{ .string = string }) catch |e| oom(e);
+    }
+
+    pub fn push_number_literal(self: *Vm, loc: std.zig.Token.Loc) !void {
         self.stack.append(self.allocator, .{
             // TODO: escape the string literal
             .number = self.allocator.dupe(u8, self.src[loc.start..loc.end]) catch |e| oom(e),
         }) catch |e| oom(e);
     }
 
-    pub fn call_builtin(self: *Vm, loc: std.zig.Token.Loc) ?[]u8 {
+    pub fn call_builtin(self: *Vm, loc: std.zig.Token.Loc) error{Vm}!void {
         const name = self.src[loc.start..loc.end];
         if (std.mem.eql(u8, name, "@out")) {
-            if (self.stack.items.len != 1) return std.fmt.allocPrint(
-                global.error_arena,
+            if (self.stack.items.len != 1) return self.generalError(
+                loc.start,
                 "@out requires 1 argument but got {}",
                 .{self.stack.items.len},
-            ) catch |e| oom(e);
-            if (self.stack.items[0] != .string) return std.fmt.allocPrint(
-                global.error_arena,
+            );
+            if (self.stack.items[0] != .string) return self.generalError(
+                loc.start,
                 "@out requires argument 1 to be of type string but got {s}",
                 .{self.stack.items[0].error_desc()},
-            ) catch |e| oom(e);
+            );
             std.io.getStdOut().writer().writeAll(self.stack.items[0].string)
                 catch |err| std.debug.panic("@out failed with {s}", .{@errorName(err)});
             self.stack.items[0].deinit(self.allocator);
             self.stack.items.len = 0;
-            return null;
         } else {
-            return std.fmt.allocPrint(global.error_arena, "unknown builtin '{s}'", .{name}) catch |e| oom(e);
+            return self.tokenError(loc.start, .unknown_builtin);
         }
     }
 };
@@ -124,30 +172,29 @@ fn lex(src: [:0]const u8, off: usize) std.zig.Token {
 //      / KEYWORD_unreachable
 //      / STRINGLITERAL
 //      / SwitchExpr
-fn PrimaryTypeExpr(src: [:0]const u8, start: usize, vm_opt: ?*Vm) ErrorOr(usize) {
+fn PrimaryTypeExpr(src: [:0]const u8, start: usize, vm_opt: ?*Vm) !usize {
     const first_token = lex(src, start);
     if (first_token.tag == .builtin) {
-        if (FnCallArguments(src, first_token.loc.end, null)) |fn_call_end| {
+        if (try FnCallArguments(src, first_token.loc.end, null)) |fn_call_end| {
             if (vm_opt) |vm| {
-                const new_end = FnCallArguments(src, first_token.loc.end, vm);
+                const new_end = try FnCallArguments(src, first_token.loc.end, vm);
                 std.debug.assert(new_end == fn_call_end);
-                if (vm.call_builtin(first_token.loc)) |msg|
-                    return .{ .err = msg };
+                try vm.call_builtin(first_token.loc);
             }
-            return .{ .ok = fn_call_end };
+            return fn_call_end;
         }
     }
     std.debug.panic("todo: token tag={s} src='{s}'", .{@tagName(first_token.tag), src[first_token.loc.start..first_token.loc.end]});
 }
 
-fn FnCallArguments(src: [:0]const u8, start: usize, vm_opt: ?*Vm) ?usize {
+fn FnCallArguments(src: [:0]const u8, start: usize, vm_opt: ?*Vm) !?usize {
     const expr_list_start = blk: {
         const token = lex(src, start);
         if (token.tag != .l_paren) return null;
         break :blk token.loc.end;
     };
 
-    const expr_list_end = ExprList(src, expr_list_start, null);
+    const expr_list_end = try ExprList(src, expr_list_start, null);
 
     const r_paren_token = lex(src, expr_list_end);
     if (r_paren_token.tag != .r_paren) {
@@ -156,18 +203,18 @@ fn FnCallArguments(src: [:0]const u8, start: usize, vm_opt: ?*Vm) ?usize {
     }
 
     if (vm_opt) |vm| {
-        const new_end = ExprList(src, expr_list_start, vm);
+        const new_end = try ExprList(src, expr_list_start, vm);
         std.debug.assert(new_end == expr_list_end);
     }
     return r_paren_token.loc.end;
 }
 
 // ExprList <- (Expr COMMA)* Expr?
-fn ExprList(src: [:0]const u8, start: usize, vm_opt: ?*Vm) usize {
+fn ExprList(src: [:0]const u8, start: usize, vm_opt: ?*Vm) !usize {
     var off = start;
-    while (Expr(src, off, null)) |expr_end| {
+    while (try Expr(src, off, null)) |expr_end| {
         if (vm_opt) |vm| {
-            const new_end = Expr(src, off, vm);
+            const new_end = try Expr(src, off, vm);
             std.debug.assert(new_end == expr_end);
         }
         off = expr_end;
@@ -180,21 +227,21 @@ fn ExprList(src: [:0]const u8, start: usize, vm_opt: ?*Vm) usize {
 }
 
 // Expr <- BoolOrExpr
-fn Expr(src: [:0]const u8, start: usize, vm_opt: ?*Vm) ?usize {
+fn Expr(src: [:0]const u8, start: usize, vm_opt: ?*Vm) !?usize {
     const token = lex(src, start);
 
     // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
     // GRAMMAR HACK for now
     // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
     if (token.tag == .string_literal) {
-        if (vm_opt) |vm| vm.push_string_literal(token.loc);
+        if (vm_opt) |vm| try vm.push_string_literal(token.loc);
         return token.loc.end;
     }
     // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
     // GRAMMAR HACK for now
     // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
     if (token.tag == .number_literal) {
-        if (vm_opt) |vm| vm.push_number_literal(token.loc);
+        if (vm_opt) |vm| try vm.push_number_literal(token.loc);
         return token.loc.end;
     }
     // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
@@ -214,16 +261,17 @@ fn testExpr(src: [:0]const u8) !void {
         .allocator = gpa.allocator()
     };
     defer vm.deinit();
-    const expr_end = switch (PrimaryTypeExpr(src, 0, &vm)) {
-        .ok => |end| end,
-        .err => |error_msg| {
-            defer global.error_arena.free(error_msg);
-            std.log.err("src '{s}' unexpectedly failed with {s}", .{src, error_msg});
+    if (PrimaryTypeExpr(src, 0, &vm)) |end| {
+        if (end != src.len) {
+            std.log.err("src '{s}' is not a PrimaryTypeExpr (end={})", .{src, end});
+        }
+    } else |vm_err| switch (vm_err) {
+        error.Vm => {
+            const err = vm.err orelse @panic("vm reported error but has none?");
+            const error_msg = err.getTestMsg();
+            std.log.err("src '{s}' had unexpected error: {s}", .{src, error_msg});
             return error.TestUnexpectedResult;
         },
-    };
-    if (expr_end != src.len) {
-        std.log.err("src '{s}' is not a PrimaryTypeExpr (end={})", .{src, expr_end});
     }
 }
 fn testError(src: [:0]const u8, expected_error: []const u8) !void {
@@ -234,13 +282,14 @@ fn testError(src: [:0]const u8, expected_error: []const u8) !void {
         .allocator = gpa.allocator()
     };
     defer vm.deinit();
-    _ = switch (PrimaryTypeExpr(src, 0, &vm)) {
-        .ok => {},
-        .err => |error_msg| {
-            defer global.error_arena.free(error_msg);
-            return std.testing.expectEqualSlices(u8, expected_error, error_msg);
+    if (PrimaryTypeExpr(src, 0, &vm)) |_| {
+        std.log.err("src '{s}' unexpectedly didn't have an error", .{src});
+        return error.TestUnexpectedResult;
+    } else |vm_err| switch (vm_err) {
+        error.Vm => {
+            const err = vm.err orelse @panic("vm reported error but has none?");
+            const actual_msg = err.getTestMsg();
+            return std.testing.expectEqualSlices(u8, expected_error, actual_msg);
         },
-    };
-    std.log.err("src '{s}' unexpectedly didn't have an error", .{src});
-    return error.TestUnexpectedResult;
+    }
 }
