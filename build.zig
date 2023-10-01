345b8011cd684c39959a513f035d3ceebdb4e706
Jonathan Marler <johnnymarler@gmail.com>
2023-10-01 12:37:50 -0600

    add "zig build test"

--- build.zig
+++ build.zig
@@ -4,19 +4,43 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const exe = b.addExecutable(.{
-        .name = "zigscript",
-        .root_source_file = .{ .path = "cmdline.zig" },
-        .target = target,
-        .optimize = optimize,
-    });
-    b.installArtifact(exe);
+    const zigscript_exe = blk: {
+        const exe = b.addExecutable(.{
+            .name = "zigscript",
+            .root_source_file = .{ .path = "cmdline.zig" },
+            .target = target,
+            .optimize = optimize,
+        });
+        b.installArtifact(exe);
 
-    const run_cmd = b.addRunArtifact(exe);
-    run_cmd.step.dependOn(b.getInstallStep());
-    if (b.args) |args| {
-        run_cmd.addArgs(args);
+        const run_cmd = b.addRunArtifact(exe);
+        run_cmd.step.dependOn(b.getInstallStep());
+        if (b.args) |args| {
+            run_cmd.addArgs(args);
+        }
+        const run_step = b.step("run", "Run the app");
+        run_step.dependOn(&run_cmd.step);
+        break :blk exe;
+    };
+
+    const test_step = b.step("test", "Run some tests");
+
+    {
+        const exe = b.addExecutable(.{
+            .name = "zigscript-test",
+            .root_source_file = .{ .path = "test.zig" },
+            .target = target,
+            .optimize = optimize,
+        });
+        const run = b.addRunArtifact(exe);
+        run.expectStdOutEqual("Hello, World!\nHello, World with Concat!\n");
+        test_step.dependOn(&run.step);
     }
-    const run_step = b.step("run", "Run the app");
-    run_step.dependOn(&run_cmd.step);
+    {
+        const run = b.addRunArtifact(zigscript_exe);
+        run.addFileSourceArg(.{ .path = "examples/helloworld.zigscript" });
+        run.expectStdOutEqual("Hello, World!\n");
+        test_step.dependOn(&run.step);
+
+    }
 }
